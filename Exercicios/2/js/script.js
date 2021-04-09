5009b18b90d6949e876e3fbedb3e4e188d5f4943
Vivian Caldas <54189003+vhcaldas@users.noreply.github.com>
2021-04-09 16:32:13 -0300

    adicionados exercícios

---
+++ Exercicios/2/js/script.js
@@ -0,0 +1,12 @@
+function mostraPrecoAptoDoisDorm() {
+    //especificar o elemento do DOM que será manipulado. DICA: botão
+
+    //evento de exibição do valor do imóvel
+  };
+  
+  function mostraPrecoAptoTresDorm() {
+
+  };
+  
+  mostraPrecoAptoDoisDorm();
+  mostraPrecoAptoTresDorm();
---
+++ Exercicios/3/script/script.js
@@ -0,0 +1,11 @@
+/*Sequência lógica
+
+1 - armazenar valores e-mail e botão
+
+*/ 
+
+botao.addEventListener("click", function(){
+
+    //evento para a exibição do texto na tela principal
+
+})
