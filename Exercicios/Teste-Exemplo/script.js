5994687bd12b54a7a183cd92b244a226937bcbbd
Mirella Raposo <mirellacruzr@gmail.com>
2021-04-20 03:06:33 -0300

    Base Formulário HTML

---
+++ Exercicios/Teste-Exemplo/script.js
@@ -0,0 +1,4 @@
+function alterarTexto() {
+    const textoAlteracao = document.getElementById("mudanca-texto").innerHTML = "Hello World"
+    return textoAlteracao;
+}
---
+++ Exercicios/Teste-Query-Selector/script.js
@@ -0,0 +1,9 @@
+function mudarCor() {
+    
+    const mudaCorTitulo = document.querySelector(".meuTitulo").style.color = "blue";
+
+    const mudaParagrafo = document.querySelector("#meu-nome").innerHTML += " e eu tenho 24 anos."
+
+    const mudaSpan = document.querySelector("span").style.textDecoration = "underline";
+    
+}
