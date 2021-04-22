6257639fe832898dd1e15754667e4de251dee0bf
Gabrielle Oliveira <biela.a09@gmail.com>
2021-04-22 19:39:41 -0300

    Adicionando 2- exercicio.

--- Exercicios/2/js/script.js
+++ Exercicios/2/js/script.js
@@ -1,12 +1,20 @@
 function mostraPrecoAptoDoisDorm() {
-    //especificar o elemento do DOM que será manipulado. DICA: botão
+  //especificar o elemento do DOM que será manipulado. DICA: botão
 
-    //evento de exibição do valor do imóvel
-  };
-  
-  function mostraPrecoAptoTresDorm() {
+  //evento de exibição do valor do imóvel
 
-  };
-  
-  mostraPrecoAptoDoisDorm();
-  mostraPrecoAptoTresDorm();
+  let getBotaoApDoisDorm = document.querySelector("#btnAptoDoisDorm");
+
+  getBotaoApDoisDorm.addEventListener("click", function mostraValor() {
+    document.getElementById("precoAptoDoisDorm").innerText =
+      "Preço do imóvel: R$ 500.000,00";
+  });
+}
+
+function mostraPrecoAptoTresDorm() {
+  document.getElementById("precoAptoTresDorm").innerHTML =
+    " Preço do imóvel: R$ 600.000,00";
+}
+
+mostraPrecoAptoDoisDorm();
+mostraPrecoAptoTresDorm();
