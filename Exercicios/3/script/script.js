655142820ac1f4402629bc325c49a9e5f2657d7e
Gabrielle Oliveira <biela.a09@gmail.com>
2021-04-22 19:43:28 -0300

    Adicionando 3 EX.

--- Exercicios/3/script/script.js
+++ Exercicios/3/script/script.js
@@ -1,11 +1,26 @@
-/*Sequência lógica
+// /*Sequência lógica
 
-1 - armazenar valores e-mail e botão
+// 1 - armazenar valores e-mail e botão
 
-*/ 
+// */
 
-botao.addEventListener("click", function(){
+// botao.addEventListener("click", function(){
 
-    //evento para a exibição do texto na tela principal
+//     //evento para a exibição do texto na tela principal
 
-})
+// })
+function mostraPrecoAptoDoisDorm() {
+  //evento de exibição do valor do imóvel
+  let getBotaoApDoisDorm = document.querySelector("#btnAptoDoisDorm");
+
+  getBotaoApDoisDorm.addEventListener("click", function mostraValor() {
+    document.getElementById("precoAptoDoisDorm").innerText =
+      "Preço do imóvel: R$ 500.000,00";
+  });
+}
+function mostraPrecoAptoTresDorm() {
+  document.getElementById("precoAptoTresDorm").innerHTML =
+    " Preço do imóvel: R$ 600.000,00";
+}
+
+mostraPrecoAptoDoisDorm();
