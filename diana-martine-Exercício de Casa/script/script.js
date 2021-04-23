e2927727c12ea7dccc31711d42e5bf03bd1d518d
dianamartine <diana_martine@outlook.com>
2021-04-23 15:16:23 -0300

    finalized all

--- diana-martine-Exercício de Casa/script/script.js
+++ diana-martine-Exercício de Casa/script/script.js
@@ -29,11 +29,14 @@ function addBook() {
   let containerTable = document.querySelector(".container-table");
   let table = document.querySelector(".tbody");
   let addTr = document.createElement("tr");
+  addTr.classList.add("tr-on");
   let addTdTitle = document.createElement("td");
   let addTdAuthor = document.createElement("td");
   let addTdIsbn = document.createElement("td");
   let addTdDate = document.createElement("td");
   let addTdPages = document.createElement("td");
+  let addTdDateI = document.createElement("td");
+  let addRemove = document.createElement("td");
 
   if (
     title.value === "" ||
@@ -43,6 +46,7 @@ function addBook() {
   ) {
     alert("Erro, insira todos os dados nos campos");
   } else {
+    // add dados na tabela
     table.appendChild(addTr);
     addTdTitle.textContent = title.value;
     addTr.appendChild(addTdTitle);
@@ -52,8 +56,19 @@ function addBook() {
     addTr.appendChild(addTdIsbn);
     addTdDate.textContent = date.value;
     addTr.appendChild(addTdDate);
-    addTdPages.innerHTML = `${pages.value}`;
+    addTdPages.innerHTML = pages.value;
     addTr.appendChild(addTdPages);
+
+    // data de inserção
+    addTdDateI.innerHTML = new Date();
+    addTr.appendChild(addTdDateI);
+
+    // remover livro
+    addRemove.innerHTML = `<a href="#"><img src="https://freeiconshop.com/wp-content/uploads/edd/minus-flat.png" alt="remover livro" style="width: 1rem;"></a>`;
+    addTr.appendChild(addRemove);
+    addRemove.addEventListener("click", function () {
+      addTr.remove();
+    });
 
     containerTable.classList.add("on");
   }
